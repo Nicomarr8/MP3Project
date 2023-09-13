e51e0c1a0e51ce9c3aa3b5c566be752e015700e3
Nicomarr8 <126726313+Nicomarr8@users.noreply.github.com>
2023-09-13 12:03:29 -0400

    added pause and unpause

--- Main.py
+++ Main.py
@@ -4,3 +4,6 @@ def playSong(directory):
     mixer.init()
     mixer.music.load(directory)
     mixer.music.play()
+
+#mixer.music.pause() - this is how to pause the music
+#mixer.music.unpause() - this is how to unpause the music

b2a61d718465e10c09d8fe5345e8c11fb15348ea
BenPare21 <fireiceben@gmail.com>
2023-09-14 14:21:40 -0400

    Update Main.py

---
+++ Main.py
@@ -0,0 +1,6 @@
+import os 
+filepath = "C:/Users/firei/OneDrive/Documents/A Software/A music folder"
+fileNames = os.listdir(filepath)
+print(fileNames)
+
+# filepath = os.listdir("./") # current dirrectry

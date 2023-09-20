26e63b5c870686e9c155fa54678862efc4c84a60
BenPare21 <fireiceben@gmail.com>
2023-09-20 11:50:51 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,26 +1,21 @@
 import os 
-import random
-
-
-filepath = "C:/Users/firei/OneDrive/Documents/A Software/A music folder"
+filepath = input("Enter filepath")
 if os.path.isdir(filepath):
+    os.chdir(filepath)
     fileNames = os.listdir(filepath) 
+
     if len(fileNames) == 0: 
         print("Folder empty \n")
     else: 
         print("Folder not empty \n")
         print(fileNames)
-        #The shuffle function 
-        random.shuffle(fileNames)
-        print (fileNames)
 else:
-    print("File doesn't exist")
+    print("File doesn't exist \n")
+   # print("Please enter a \n")
 
 
 """ To do list
 Need a click and drag file box UI to change the file path to the dragged
 file.
-Need button that says shuffle
-checked one condition
-
+C:/Users/firei/OneDrive/Documents/A Software/A music folder
 """

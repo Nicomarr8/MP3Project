f938e970873a8b59c344666f1a31de6943c98984
Goon254 <127537728+Goon254@users.noreply.github.com>
2023-09-22 11:51:07 -0400

    Settings File
    create a file for saving settings along with some beginning settings features, have a function that reads the information and outputs it as an array of the values. (visual themes for the app, liked songs could be in there, account specific stuff)

---
+++ Main.py
@@ -0,0 +1,81 @@
+import json
+
+# default settings dictionary
+DEFAULT_SETTINGS = {
+    "visual_theme": "default",
+    "liked_songs": [],
+    "audio_settings": {
+        "volume": 50,
+        "equalizer": {
+            "bass": 0,
+            "treble": 0
+        }
+    },
+    "preferences": {
+        "language": "English",
+        "notifications": True
+    },
+    "about_info": {
+        "INformation about the app"
+           }
+}
+
+# load settings from the JSON file
+def load_settings():
+    try:
+        with open('settings.json', 'r') as file:
+            settings = json.load(file)
+    except FileNotFoundError:
+        settings = DEFAULT_SETTINGS
+    return settings
+
+# Save settings to the JSON file
+def save_settings(settings):
+    with open('settings.json', 'w') as file:
+        json.dump(settings, file, indent=4)
+
+#Load settings at the beginning of your program
+current_settings = load_settings()
+
+# Access and update settings as needed
+visual_theme = current_settings["visual_theme"]
+liked_songs = current_settings["liked_songs"]
+username = current_settings["account_info"]["username"]
+volume = current_settings["audio_settings"]["volume"]
+bass = current_settings["audio_settings"]["equalizer"]["bass"]
+treble = current_settings["audio_settings"]["equalizer"]["treble"]
+language = current_settings["preferences"]["language"]
+notifications = current_settings["preferences"]["notifications"]
+app_version = current_settings["about_info"]["version"]
+developer = current_settings["about_info"]["developer"]
+website = current_settings["about_info"]["website"]
+
+# Function to change settings
+def change_settings(new_settings):
+    for key, value in new_settings.items():
+        if key in current_settings:
+            current_settings[key] = value
+        else:
+            print(f"Invalid setting: {key}")
+
+# Changing settings
+new_settings = {
+    "visual_theme": "dark",
+    "audio_settings": {
+        "volume": 75,
+        "equalizer": {
+            "bass": 2,
+            "treble": -1
+        }
+    },
+    "preferences": {
+        "language": "French",
+        "notifications": False
+    }
+}
+
+# Apply the new settings
+change_settings(new_settings)
+
+# Save the updated settings
+save_settings(current_settings)

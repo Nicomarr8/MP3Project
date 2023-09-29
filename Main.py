93696cdbff4a793d1d8d721b8ad91ef31120a708
Nicomarr8 <126726313+Nicomarr8@users.noreply.github.com>
2023-09-29 16:01:16 -0400

    don't try and run this, I'm just saving while I'm in the middle of something

--- Main.py
+++ Main.py
@@ -132,7 +132,10 @@ class Window(tkinter.Tk):
         self.title("")
         self.geometry('600x400')
         self.configure(background = "white")
-        self.state('zoomed')
+        #self.state('zoomed')
+        self.buttonImages = {}
+        self.buttons = {}
+        self.canvases = {}
         
         #frames
         self.left_frame = tkinter.Frame( width = 150, height = 150, bg = "white")
@@ -154,48 +157,41 @@ class Window(tkinter.Tk):
         #images
         self.img_1=Image.open("img\\mp3.png")
         self.img_1 = self.img_1.resize((130,130))
-        self.img_1= ImageTk.PhotoImage(self.img_1)
-        self.app_image = tkinter.Label(self.left_frame, height =130, image=self.img_1, padx=10, bg="white")
+        self.app_image = tkinter.Label(self.left_frame, height =130, image=ImageTk.PhotoImage(self.img_1), padx=10, bg="white")
 
         #play button
-        self.img_2=Image.open("img\\play.png")
-        self.img_2 = self.img_2.resize((30,30))
-        self.img_2=ImageTk.PhotoImage(self.img_2)
-        self.play_button = tkinter.Button(self.down_frame, height =40,width = 40, image=self.img_2, padx=10, bg="white")
+        # self.buttonImages["play"] = Image.open("img\\play.png")
+        # self.buttons["play"] = tkinter.Button(self.down_frame, height =40,width = 40, padx=10, bg="white")
 
 
         #prev button
-        self.img_3=Image.open("img\\previous.png")
-        self.img_3 = self.img_3.resize((30,30))
-        self.img_3=ImageTk.PhotoImage(self.img_3)
-        self.prev_button = tkinter.Button(self.down_frame, height=40, width=40, image=self.img_3, padx=10, bg="white")
+        self.buttonImages["previous"] = Image.open("img\\previous.png")
+        self.buttons["previous"] = tkinter.Button(self.down_frame, height =40,width = 40, padx=10, bg="white")
 
         #next button
         self.img_4=Image.open("img\\next.png")
         self.img_4 = self.img_4.resize((30,30))
-        self.img_4=ImageTk.PhotoImage(self.img_4)
-        self.next_button = tkinter.Button(self.down_frame, height =40,width = 40, image=self.img_4, padx=10, bg="white")
+        self.temp4Imgs = ImageTk.PhotoImage(self.img_4)
+        self.next_button = tkinter.Button(self.down_frame, height =40,width = 40, image=self.temp4Imgs, padx=10, bg="white")
 
 
         #pause button
         self.img_5=Image.open("img\\pause.png")
         self.img_5 = self.img_5.resize((30,30))
-        self.img_5=ImageTk.PhotoImage(self.img_5)
-        self.pause_button = tkinter.Button(self.down_frame, height =40,width = 40, image=self.img_5, padx=10, bg="white")
+        self.temp4Imgs = ImageTk.PhotoImage(self.img_5)
+        self.pause_button = tkinter.Button(self.down_frame, height =40,width = 40, image=self.temp4Imgs, padx=10, bg="white")
 
 
         #continue button
         self.img_6=Image.open("img\\continue.png")
         self.img_6 = self.img_6.resize((30,30))
-        self.img_6=ImageTk.PhotoImage(self.img_6)
-        self.continue_button = tkinter.Button(self.down_frame, height =40,width = 40, image=self.img_6, padx=10, bg="white")
+        self.continue_button = tkinter.Button(self.down_frame, height =40,width = 40, image=ImageTk.PhotoImage(self.img_6), padx=10, bg="white")
 
 
         #stop button
         self.img_7=Image.open("img\\stop.png")
         self.img_7 = self.img_7.resize((30,30))
-        self.img_7=ImageTk.PhotoImage(self.img_7)
-        self.stop_button = tkinter.Button(self.down_frame, height =40,width = 40, image=self.img_7, padx=10, bg="white")
+        self.stop_button = tkinter.Button(self.down_frame, height =40,width = 40, image=ImageTk.PhotoImage(self.img_7), padx=10, bg="white")
 
         # seek bar
         self.seek= tkinter.Scale(self.down_frame, from_=0, to =100, orient="horizontal")
@@ -204,6 +200,7 @@ class Window(tkinter.Tk):
         self.volume= tkinter.Scale(self.down_frame, from_=0, to =100, orient="horizontal")
 
         #refresh to put everythign in place
+        self.bind("<Configure>",self.configImages)
         self.refresh()
 
     def refresh(self):
@@ -212,36 +209,66 @@ class Window(tkinter.Tk):
         self.right_frame.grid(row=0, column=1, padx=0, sticky="nsew")
         self.right_frame.grid_rowconfigure(0, weight=1)
         self.right_frame.grid_columnconfigure(0, weight=1)
-        self.down_frame.grid(row=1, column=0,columnspan=3, padx=0, pady=1, sticky="nsew")
-        print(self.grid_size())
+        self.down_frame.grid(row=1, column=0,columnspan=2, padx=0, pady=1, sticky="nsew")
 
         #listbox
         self.listbox.grid(row=0, column=0, pady=10, padx=10, sticky="nsew")
 
         #scrollbar
-        self.my_scrollbar.grid(row=0, column=1, sticky="ns")
+        self.my_scrollbar.grid(row=0, column=1, sticky="nse")
 
         #Images
-        self.app_image.grid(row=0,column=0,sticky="nsew")
-        self.play_button.grid(row=0,column=2, sticky="s")
-        self.prev_button.grid(row=0,column=1, sticky="s")
-        self.next_button.grid(row=0,column=3, sticky="s")
-        self.pause_button.grid(row=0,column=4, sticky="s")
-        self.continue_button.grid(row=0,column=5, sticky="s")
-        self.stop_button.grid(row=0,column=6, sticky="s")
+        self.app_image.grid(row=1,column=0,sticky="nsew")
+        self.genPlayButton(0.4)
+        self.canvases["play"].grid(row=1,column=0)
+        
+        # for i in range(len(self.buttons)):
+        #     self.buttons[list(self.buttons)[i]].grid(row=1,column=2, sticky="nsew")
+        # self.next_button.grid(row=1,column=3, sticky="nsew")
+        # self.pause_button.grid(row=1,column=4, sticky="nsew")
+        # self.continue_button.grid(row=1,column=5, sticky="nsew")
+        # self.stop_button.grid(row=1,column=6, sticky="nsew")
 
         #seek bar
-        self.seek.grid(row=0, column=7)
+        self.seek.grid(row=0, column=0,columnspan=7,sticky="nsew")
 
         #volume slider
-        self.volume.grid(row=0, column=8)
+        self.volume.grid(row=0, column=7,columnspan=3,sticky="nsew")
 
+        #makes all of the frames expand to fit the window
+        #parent window
+        for i in range(self.grid_size()[0]):
+            self.grid_columnconfigure(i,weight=1)
+        for i in range(self.grid_size()[1]):
+            self.grid_rowconfigure(i,weight=1)
 
-    def configure_frames(self):
-        self.grid_rowconfigure(0, weight=1)  # Allow the top row (containing left and right frames) to expand vertically
-        self.grid_columnconfigure(0, weight=1)  # Allow the first column (containing left frame) to expand horizontally
-        self.grid_columnconfigure(1, weight=1)  # Allow the second column (containing right frame) to expand horizontally
-        self.grid_rowconfigure(1, weight=1)  # Allow the bottom row (containing down frame) to expand vertically
+    #generates the play button image
+    def genPlayButton(self,factor):
+        self.canvases["play"] = tkinter.Canvas(self.down_frame,width=100*factor,height=100*factor,background="SystemButtonFace",borderwidth=2,relief="raised")
+        self.canvases["play"].create_oval(10*factor,10*factor,97*factor,97*factor, outline="black", fill="white", width=2)
+        self.canvases["play"].create_polygon([40*factor,25*factor,80*factor,50*factor,40*factor,80*factor],outline="black",fill="white",width=2)
+        self.canvases["play"].grid(row=0,column=0)
+        #the function for clicking on the play button (just animation right now)
+        def onClick(event):
+            event.widget.configure(relief="sunken")
+            event.widget.delete("all")
+            event.widget.create_oval(10*factor+2,10*factor+2,97*factor+2,97*factor+2, outline="black", fill="white", width=2)
+            event.widget.create_polygon([40*factor+2,25*factor+2,80*factor+2,50*factor+2,40*factor+2,80*factor+2],outline="black",fill="white",width=2)
+        self.canvases["play"].bind("<ButtonPress-1>",onClick)
+        #the function for releasing the play button (just animation right now)
+        def onRelease(event):
+            event.widget.configure(relief="raised")
+            event.widget.delete("all")
+            event.widget.create_oval(10*factor,10*factor,97*factor,97*factor, outline="black", fill="white", width=2)
+            event.widget.create_polygon([40*factor,25*factor,80*factor,50*factor,40*factor,80*factor],outline="black",fill="white",width=2)
+        self.canvases["play"].bind("<ButtonRelease-1>",onRelease)
+
+    def configImages(self,event):
+        for i in range(len(self.buttonImages)):
+            temp4Imgs = self.buttonImages[list(self.buttonImages)[i]].resize((event.width,event.height))
+            temp4Imgs = ImageTk.PhotoImage(temp4Imgs)
+            self.buttons[list(self.buttonImages)[i]].configure(image=temp4Imgs)
+
 
 
 #configure_frames()  # Call the configure_frames function to make the frames resizable
